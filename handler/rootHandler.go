a1d42aac27a70a8d32ec43e86424e58e9088e3b0
justin caseof <justin.caseof@gmail.com>
2020-11-18 11:32:14 +0100

    * make test working

--- handler/rootHandler.go
+++ handler/rootHandler.go
@@ -4,14 +4,13 @@ import (
 	"encoding/xml"
 	"fmt"
 	"net/http"
-	"ygost/helper"
+	"ygost/model"
 )
 
 func RootHandler(writer http.ResponseWriter, request *http.Request) {
 	fmt.Println("RootHandler")
-	items := helper.ParseFile("_examples/00_root.xml")
 
-	result, err := xml.Marshal(items)
+	result, err := xml.Marshal(model.Root)
 	if err != nil {
 		fmt.Println("cannot marshall")
 	}
--- main_test.go
+++ main_test.go
@@ -1,56 +1,81 @@
 package main
 
 import (
+	"encoding/xml"
 	"fmt"
 	"io/ioutil"
-	"log"
 	"net/http/httptest"
 	"os"
 	"testing"
 	"ygost/handler"
 	"ygost/helper"
+	"ygost/model"
 )
 
 func TestParseSuccessOfRoot(t *testing.T) {
-	fmt.Println(helper.ParseFile("_examples/00_root.xml"))
+	fmt.Println(helper.ParseFile("_examples/DO_NOT_CHANGE_TESTS_DEPEND_ON_IT/00_root.xml"))
 }
 
 func TestParseSuccessOfMyStations(t *testing.T) {
-	fmt.Println(helper.ParseFile("_examples/01_my_stations.xml"))
+	fmt.Println(helper.ParseFile("_examples/DO_NOT_CHANGE_TESTS_DEPEND_ON_IT/01_my_stations.xml"))
 }
 
 func TestParseSuccessOfSubdirXYZ(t *testing.T) {
-	fmt.Println(helper.ParseFile("_examples/02_my_stations-GROUPNAME.xml"))
+	fmt.Println(helper.ParseFile("_examples/DO_NOT_CHANGE_TESTS_DEPEND_ON_IT/02_my_stations-GROUPNAME.xml"))
 }
 
 func TestRootHandler(t *testing.T) {
 	rec := httptest.NewRecorder()
 
+	// setup mock data
+	model.Root = model.ListOfItems{
+		ItemCount: -1, // looks like to be some kind of default for root folder
+		Items: []model.Item{
+			model.Item{
+				ItemType:     "Dir",
+				Title:        "Radiobrowser",
+				UrlDir:       "http://192.168.178.61/ycast/radiobrowser/?vtuner=true",
+				UrlDirBackUp: "http://192.168.178.61/ycast/radiobrowser/?vtuner=true",
+				DirCount:     4,
+			},
+			model.Item{
+				ItemType:     "Dir",
+				Title:        "My Stations",
+				UrlDir:       "http://192.168.178.61/ycast/my_stations/?vtuner=true",
+				UrlDirBackUp: "http://192.168.178.61/ycast/my_stations/?vtuner=true",
+				DirCount:     4,
+			},
+		},
+	}
+
+	// INVOKE !
 	handler.RootHandler(rec, nil)
 
-	xmlFile, err := os.Open("_examples/00_root.xml")
+	// read expected data
+	xmlFile, err := os.Open("_examples/DO_NOT_CHANGE_TESTS_DEPEND_ON_IT/00_root.xml")
 	if err != nil {
 		t.Fatalf("Cannot read test data")
 	}
 	defer xmlFile.Close()
-	byteValue, _ := ioutil.ReadAll(xmlFile)
+	expectedContect, _ := ioutil.ReadAll(xmlFile)
+	var listOfItemsExpected model.ListOfItems
+	xml.Unmarshal(expectedContect, &listOfItemsExpected)
+
+	// read response and marshll
 	responseBytes, _ := ioutil.ReadAll(rec.Body)
+	var listOfItemsInResponse model.ListOfItems
+	xml.Unmarshal(responseBytes, &listOfItemsInResponse)
 
-	if !Equal(byteValue, responseBytes) {
-		log.Fatalf("Not equal!")
+	if listOfItemsExpected.ItemCount != listOfItemsInResponse.ItemCount {
+		t.Fail()
 	}
-}
 
-// Equal tells whether a and b contain the same elements.
-// A nil argument is equivalent to an empty slice.
-func Equal(a, b []byte) bool {
-	if len(a) != len(b) {
-		return false
+	if len(listOfItemsExpected.Items) != 2 {
+		t.Fail()
 	}
-	for i, v := range a {
-		if v != b[i] {
-			return false
-		}
+
+	if len(listOfItemsExpected.Items) != len(listOfItemsInResponse.Items) {
+		t.Fail()
 	}
-	return true
+
 }
